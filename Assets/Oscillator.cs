162040c0b3c9b83cf70ff01fe32c38761b53a900
Matthew Dauphinais <matthewjdauphinais@gmail.com>
2018-04-28 21:00:40 -0400

    STuffs

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(0, 1)] [SerializeField] float movementFactor; //Tells us when at max distance
+    Vector3 startPos;
+
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+
+	void Update ()
+    {
+        Vector3 offSet;
+        transform.position = startPos + offSet;
+	}
+}
